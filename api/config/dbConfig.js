6cfd0764006daf7c52657e3079cbaa3550a68b5d
Abdurleone <abyyyusuf@gmail.com>
2025-02-05 15:42:55 +0300

    DB Configs

--- api/config/dbConfig.js
+++ api/config/dbConfig.js
@@ -11,8 +11,10 @@ const connectDB = async () => {
     try {
         const conn = await connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
+        console.log('Database connection successful');
     } catch (error) {
         console.error(`Error: ${error.message}`);
+        console.log('Database connection unsuccessful');
         process.exit(1); // Exit process with failure
     }
 };
