510032e7cd0838020d2219ab0eb1d713d6068fec
Abdurleone <abyyyusuf@gmail.com>
2025-02-08 17:38:37 +0300

    More ammendments

--- lab-chatbot-ui/src/index.js
+++ lab-chatbot-ui/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import AppRoutes from './App';
+import AppRoutes from './App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
