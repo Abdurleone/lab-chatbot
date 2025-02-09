1c045bb72a820fdcef957cafc1a373e31b689d49
Abdurleone <abyyyusuf@gmail.com>
2025-02-09 12:04:55 +0300

    Added

--- lab-chatbot-ui/src/components/Navbar.jsx
+++ lab-chatbot-ui/src/components/Navbar.jsx
@@ -4,12 +4,13 @@ import './Navbar.css';
 function Navbar() {
   return (
     <nav className="navbar">
-      <h1>Lab Chatbot</h1>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/chatbox">Chatbox</Link></li>
         <li><Link to="/results">Results</Link></li>
+        <li><Link to="/tests">Tests</Link></li>
+        <li><Link to="/appointments">Appointments</Link></li>
       </ul>
     </nav>
   );
