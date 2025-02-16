e41f192961f2acb3685b84206eca1652a05f39eb
Abdurleone <abyyyusuf@gmail.com>
2025-02-16 15:26:15 +0300

    Updated files

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,19 @@
-module.exports = {
-    transform: {
-      "^.+\\.jsx?$": "babel-jest", // Use babel-jest to transpile JavaScript and JSX files
-    },
-    testEnvironment: "node", // Use Node.js environment for testing
-    roots: ["<rootDir>/src"], // Specify the root folder where Jest looks for tests
-    moduleFileExtensions: ["js", "jsx"], // Recognize JavaScript and JSX extensions
-    testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"], // Match test files
-    verbose: true, // Enable detailed test logs
-    collectCoverage: true, // Collect test coverage
-    coverageDirectory: "<rootDir>/coverage", // Output directory for coverage reports
-    clearMocks: true, // Automatically reset mocks between tests
+const config = {
+  transform: {
+    "^.+\\.m?[jt]sx?$": "babel-jest",
+  },
+  extensionsToTreatAsEsm: [".js"],
+  testEnvironment: "node",
+  roots: ["<rootDir>/src"],
+  moduleFileExtensions: ["js", "jsx"],
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)",
+  ],
+  verbose: true,
+  collectCoverage: true,
+  coverageDirectory: "<rootDir>/coverage",
+  clearMocks: true,
 };
-  
+
+export default config;  
